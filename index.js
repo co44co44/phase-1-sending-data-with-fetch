ab54e2563d615137df899816ec91a88c02be9fe0
Maria Cristina Ordonez <crisordonez44@gmail.com>
2022-10-21 10:28:01 -0500

    commit tree 28898334f00e023cc3e7de1fa581ae9ab389d118

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Add your code here
+
+// test1 passed!
+
+
+const formData ={
+    name:"Steve",
+    email:"steve@steve.com",
+};
+
+function submitData (name, email){
+    return fetch ('http://localhost:3000/users',{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),     
+    })
+    
+    .then(function (response) {
+        return response.json();
+      })
+    .then(function (object) {
+        console.log(object);
+
+    document.querySelector("body").innerHTML=object.id;
+    
+      })
+
+    .catch(function (error) {
+        alert('Unauthorized Access');
+        console.log(error.message);
+        document.querySelector("body").innerHTML=error.message;
+    })
+    
+      
+};
+
+
